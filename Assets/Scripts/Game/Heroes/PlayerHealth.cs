f57d4fbb00c8d804f7fb68889877d8ee1844858f
arthur-lava <arthurehrenburg@gmail.com>
2023-09-08 12:19:38 +0200

    imported models and sprites and added game over scene

--- Assets/Scripts/Game/Heroes/PlayerHealth.cs
+++ Assets/Scripts/Game/Heroes/PlayerHealth.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
     public static PlayerHealth Instance { get; private set; }
     public int HP { get; private set; }
-
+    [SerializeField] private Image heart1;
+    [SerializeField] private Image heart2;
     private void Awake() {
         if (Instance == null) Instance = this;
         else Destroy(this.gameObject);
@@ -29,10 +31,13 @@ public class PlayerHealth : MonoBehaviour {
         if (HP <= 1) {
             Death();
         }
+        if(HP == 1) {
+
+        }
     }
 
     private void Death() {
-        // Show End UI
+        SceneManager.Instance.SwitchScene("GameOver");
     }
 
     private void OnDestroy()
